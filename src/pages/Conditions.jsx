6b51a76c2b1a821837897731d4c231965596c8f6
Vedant <vedantshiradhonkar3000@gmail.com>
2023-11-04 12:51:20 +0530

    css changes to terms

--- src/pages/Conditions.jsx
+++ src/pages/Conditions.jsx
@@ -86,13 +86,73 @@ return (
             <br />
         <h1>15. Entire Agreement</h1>
             <p>These terms and the rental agreement constitute the entire agreement between you and us.</p>
-        
-        <p style={{color:"red", fontSize: "2rem"}}>*for more details contact customer care service</p>
         <br/>
+        <h1>16. Age Restrictions</h1>
+            <p>We only rent vehicles to drivers who meet our minimum and maximum age requirements.</p>
+
+            <br />
+        <h1>17. Additional Drivers</h1>
+            <p>If you want to add an additional driver, please inform us in advance and provide their information.</p>
+
+            <br />
+        <h1>18. Vehicle Availability</h1>
+            <p>We strive to provide the vehicle you've reserved, but in rare cases, we may substitute it with a similar model.</p>
+
+            <br />
+        <h1>19. Mileage Limits</h1>
+            <p>Your rental agreement will specify any mileage limits. Exceeding these limits may result in extra charges.</p>
+
+            <br />
+        <h1>20. Roadside Assistance</h1>
+            <p>We offer roadside assistance in case of emergencies or breakdowns during your rental period.</p>
+
+            <br />
+        <h1>21. Return Policy</h1>
+            <p>Return your rental car on or before the specified date and time to avoid late fees.</p>
+
+            <br />
+        <h1>22. Child Safety Seats</h1>
+            <p>We provide child safety seats upon request to ensure the safety of young passengers.</p>
+
+            <br />
+        <h1>23. GPS Navigation</h1>
+            <p>For your convenience, we offer GPS navigation systems to help you find your way during your trip.</p>
+
+            <br />
+        <h1>24. Smoking Policy</h1>
+            <p>Smoking is strictly prohibited in our rental vehicles. A cleaning fee may apply if this policy is violated.</p>
+
+            <br />
+        <h1>25. Pets and Animals</h1>
+            <p>To maintain cleanliness and prevent allergies, pets and animals are not allowed in our vehicles.</p>
+
+            <br />
+        <h1>26. Lost or Damaged Items</h1>
+            <p>We are not responsible for any personal items left in the rental vehicle, and fees may apply for lost or damaged items.</p>
+
+            <br />
+        <h1>27. Maintenance Responsibility</h1>
+            <p>You are responsible for checking the vehicle's fluids and tire pressure during your rental and reporting any issues.</p>
+
+            <br />
+        <h1>28. Fuel Refilling</h1>
+            <p>Return the vehicle with the same fuel level as at the start of the rental. A refueling fee may apply if not followed.</p>
+
+            <br />
+        <h1>29. International Travel</h1>
+            <p>If you plan to travel to another country, please inform us in advance to ensure proper documentation.</p>
+
+            <br />
+        <h1>30. Environmental Responsibility</h1>
+            <p>We encourage responsible and eco-friendly driving to reduce the environmental impact of our rentals.</p>
+        <br/>
+
+        <p style={{color:"red", fontSize: "2rem"}}>*for more details contact customer care service</p>
         <br/>
         
         <p style={{color:"black", fontWeight: "700"}}>By using our service, you agree to these terms and conditions. If you do not agree, please do not use our services.</p>
-        
+        <br/>
+        <br/>
     </div>
         <Banner/>
         <Footer />
