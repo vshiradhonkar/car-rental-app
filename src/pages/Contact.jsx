3606d89b6792781087e205b8a8bbbecd25c79b02
Vedant <vedantshiradhonkar3000@gmail.com>
2023-11-11 12:16:21 +0530

    added contact page backend to firebase to store till 2026

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroPages from "../components/HeroPages";
 import Footer from "../components/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,8 +10,46 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import ScrollTop from "../components/ScrollTop";
 import {motion} from "framer-motion";
+import { firestore } from "../firebase";
+
 
 function Contact() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [question, setQuestion] = useState("");
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate if required fields are filled
+    if (!fullName || !email || !question) {
+      // Handle validation error, e.g., show an alert
+      alert("Please fill in all required fields");
+      return;
+    }
+
+    try {
+      // Store user's inquiry in Firestore
+      await firestore.collection("inquiries").add({
+        fullName,
+        email,
+        question,
+      });
+
+      // Reset form fields after successful submission
+      setFullName("");
+      setEmail("");
+      setQuestion("");
+
+      // Display a success message or redirect the user
+      alert("Message sent successfully!");
+    } catch (error) {
+      console.error("Error sending message:", error);
+      // Handle error, e.g., show an error message to the user
+    }
+  };
+
   return (
     <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
       <section className="contact-page">
@@ -39,22 +77,21 @@ function Contact() {
               </a>
             </div>
             <div className="contact-div_form">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <label>
                   Full Name <b>*</b>
                 </label>
-                <input type="text" placeholder='E.g: "Jack Smith"'></input>
+                <input type="text" placeholder='E.g: "Jack Smith"' value={fullName} onChange={(e) => setFullName(e.target.value)}/>
 
                 <label>
                   Email <b>*</b>
                 </label>
-                <input type="email" placeholder="youremail@example.com"></input>
+                <input type="email" placeholder="youremail@example.com"  value={email} onChange={(e) => setEmail(e.target.value)} />
 
                 <label>
                   Tell us about it <b>*</b>
                 </label>
-                <textarea placeholder="Write Here.."></textarea>
-
+                <textarea placeholder="Write Here.."  value={question}  onChange={(e) => setQuestion(e.target.value)}></textarea>
                 <button type="submit">
                   <FontAwesomeIcon icon={faEnvelopeOpenText} />
                   &nbsp; Send Message
