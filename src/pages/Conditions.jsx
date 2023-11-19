bca47135a5dfd3c455e73cd7c88e5009b303281a
Vedant <vedantshiradhonkar3000@gmail.com>
2023-11-19 11:22:07 +0530

    issue fix commit

--- src/pages/Conditions.jsx
+++ src/pages/Conditions.jsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 import Banner from '../components/Banner';
 
 
+
 function Conditions() {
 return (
     <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0, transition: {duration:0.1}}} className='terms'>
